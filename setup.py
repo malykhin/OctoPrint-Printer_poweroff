dba66dbbb9359c849a68893a04392bfe75dd11ad
Denys Malykhin <malykhin@ukr.net>
2023-01-11 17:38:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ plugin_package = "octoprint_printer_poweroff"
 plugin_name = "OctoPrint-Printer_poweroff"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.1.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -34,7 +34,7 @@ plugin_url = "https://github.com/malykhin/OctoPrint-Printer_poweroff"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["meross_iot"]
+plugin_requires = ["meross_iot>=0.4.5.9"]
 
 # --------------------------------------------------------------------------------------------------------------------
 # More advanced options that you usually shouldn't have to touch follow after this point
